427eccf2ceab7b615674c0250990739fcecbb6c8
anthonyfundaro <115553733+anthonyfundaro@users.noreply.github.com>
2022-12-11 12:05:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Anthony Fundaro"; // update the part BETWEEN the "quotes"
+let userName = "anthonyfundaro"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
